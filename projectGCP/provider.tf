325265b58223c92042f7a68a91c2581a8e63c1eb
akhil0703-gif <akhilkumar0703@gmail.com>
2026-03-16 12:50:54 +0530

    Update provider.tf

--- projectGCP/provider.tf
+++ projectGCP/provider.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+  backend "gcs" {
+    bucket = "testproject-488314-tfstate" # Ensure this bucket is created first
+    prefix = "terraform/state"
+  }
+}
+
 provider "google" {
   project = var.project_id
   region  = var.region
