a06af577c64eba8b048e949cfd376f8c8247e2d1
akhil0703-gif <akhilkumar0703@gmail.com>
2026-03-22 14:13:36 +0530

    Delete modules/storage directory

--- modules/storage/output.tf
+++
@@ -1,4 +0,0 @@
-output "bucket_url" {
-  description = "The URL of the created storage bucket"
-  value       = google_storage_bucket.hub_storage.url
-}
--- modules/storage/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "bucket_name" { 
-  type        = string
-  description = "The name of the Google Cloud Storage bucket"
-}
-
-variable "location" { 
-  type        = string
-  description = "The location of the bucket"
-}
